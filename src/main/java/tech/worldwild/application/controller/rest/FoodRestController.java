8f57bef93bfd7c28903eb569148ddec4e3ec3d2f
mantogia <mantogia@students.zhaw.ch>
2022-12-06 00:27:03 +0100

    clean version

--- src/main/java/tech/worldwild/application/controller/rest/FoodRestController.java
+++ src/main/java/tech/worldwild/application/controller/rest/FoodRestController.java
@@ -1,7 +1,5 @@
 package tech.worldwild.application.controller.rest;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import tech.worldwild.application.entities.Food;
 import tech.worldwild.application.repositories.FoodRepository;
